29adf2e5916184e8e3de5e2ef4e0f4d80703bc8f
jyotigutal <jyoti.gutal@gmail.com>
2023-09-20 12:07:49 +0530

    Variable Assignment 1
    Variable Assignment 1

---
+++ 01_swapVarible.js
@@ -0,0 +1,11 @@
+
+var temp,num1,num2;
+num1=100;
+num2=200;
+console.log("============Before Swap values are==========");
+console.log("num1="+num1, "and num2="+num2);
+temp=num1;
+num1=num2;
+num2=temp;
+console.log("============After Swap values are==========");
+console.log("num1="+num1,"and num2="+num2);
---
+++ 01_variableAssign.js
@@ -0,0 +1,26 @@
+
+var myFullName;
+console.log("Before Initialization = "+myFullName);
+myFullName ="Jyoti Gutal";
+console.log("My First and Last Name = "+myFullName);
+myFullName= "Jyoti Namedeo Gutal";
+console.log("My updated name is = "+myFullName);
+
+var myLove= "I love only JS";
+console.log("Value Initialization of MyLove = "+myLove);
+
+var firstName ="Jyoti";
+var lastName = "Gutal";
+var age ="30yr's";
+var address="Pune";
+var pinCode="411028";
+var gender="Female";
+var isMarried="Yes";
+console.log("First Name = "+firstName);
+console.log("Last Name = "+lastName);
+console.log("Age = "+age);
+console.log("Address = "+address);
+console.log("Pincode = "+pinCode);
+console.log("Gender = "+gender);
+console.log("Is Married = "+isMarried);
+
