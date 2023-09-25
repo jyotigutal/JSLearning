9c9369983d34f46ce4acab4ea1f535164a9282f6
jyotigutal <jyoti.gutal@gmail.com>
2023-09-25 13:14:39 +0530

    String  Bank Assignment
    String  Bank Assignment

---
+++ 03_funAssign02.js
@@ -0,0 +1,12 @@
+
+
+function bankDetails(bankName,accountNum,location,pinCode){
+    console.log("Bank name is :", bankName ,"|| Account number is :" , accountNum, "|| Location is :" , location, "|| Pin Code is :" , pinCode);
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    
+}
+console.log("====================Step 1.2====================");
+
+bankDetails("CITI Bank",3456782345,'Pune',431202);
+bankDetails("Axis Bank",7856782345,'Mumbai',441202);
+bankDetails("HDFC Bank",8956782345,'Pune',631202,'Open');
---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,18 @@
+
+
+function stringBasics(){
+    
+}
+stringBasics();
+
+console.log("===================Step 1====================");
+var strCompanyName = "Tach Mahindra";
+console.log("My dream company is", strCompanyName);
+
+console.log("===================Step 2.1====================");
+var hobby1 = "Reading",hobby2=", Traveling",hobby3=", Programming";
+console.log("My Hobbies are ",hobby1 , hobby2 , hobby3);
+
+console.log("===================Step 2.2====================");
+var totalChars = hobby1+hobby2+hobby3; 
+console.log("Total number of characters : ",totalChars.length);
