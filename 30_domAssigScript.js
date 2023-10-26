225b2853042ece8663dc0c5a52d6bc4a0571a927
jyotigutal <jyoti.gutal@gmail.com>
2023-10-26 13:07:04 +0530

    Dom Assignment
    Dom Assignment

---
+++ 30_domAssigScript.js
@@ -0,0 +1,13 @@
+
+console.log(`======= DOM API querySelector =======`);
+const mainHeading = document.querySelector('h1');
+console.log(mainHeading.innerHTML);
+
+console.log(`======= DOM API querySelectorAll =======`);
+const secondAdvantage = document.querySelectorAll('li');
+console.log(secondAdvantage[1].innerHTML);
+
+console.log(`======= DOM API querySelector =======`);
+const advDiv = document.querySelector('#advantages');
+console.log(advDiv.innerHTML);
+
